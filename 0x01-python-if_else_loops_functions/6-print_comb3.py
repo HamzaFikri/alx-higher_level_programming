5a8f817e48ddf98ec383576232c642c6391bb87f
HamzaFikri <mohammed.hamza.fikri@gmail.comgit config --global user.name HamzaFikrigit config --global user.email mohammed.hamza.fikri@gmail.com>
2023-06-06 12:08:23 -0700

    Task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-for a in range(0, 9):
-    for b in range(1 + a, 10):
-        print("{:d}{:d}".format(a, b), end="")
-        if (a + b) is not 17:
-            print(",", end=" ")
-print()
+for a in range(0, 10):
+    for b in range(0, 10):
+        if a != b and a < b:
+            print("{:d}{:d}".format(a, b), end="")
+            if a == 8 and b == 9:
+                print("")
+            else:
+                print(", ", end="")
