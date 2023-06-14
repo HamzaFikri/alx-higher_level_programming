8b73335b48e52fb7de6095d23262a5f17ae1398b
HamzaFikri <mohammed.hamza.fikri@gmail.comgit config --global user.name HamzaFikrigit config --global user.email mohammed.hamza.fikri@gmail.com>
2023-06-14 14:49:37 -0700

    Tasks more data structure

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        return (max(a_dictionary))
-    else:
-        return None
+        value = 0
+        for i, j, in a_dictionary.items():
+            if j > value:
+                value = j
+                name = i
+        return name
