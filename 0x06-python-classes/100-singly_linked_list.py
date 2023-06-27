3b75b3d8ab73bf71b91c941a40c5c2c32e24183e
HamzaFikri <mohammed.hamza.fikri@gmail.comgit config --global user.name HamzaFikrigit config --global user.email mohammed.hamza.fikri@gmail.com>
2023-06-27 15:58:09 -0700

    Advanced tasks paython class

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,22 +1,88 @@
 #!/usr/bin/python3
-import dis
-import math
+class Node:
+    """ Class to each node of a Singly linked list
+    """
 
+    def __init__(self, data, next_node=None):
+        """ Class constructor
 
-class MagicClass:
-    def __init__(self, radius=0):
-        self.__radius = 0
-
-        if type(radius) is not int and type(radius) is not float:
-            raise TypeError("radius must be a number")
-        self.__radius = radius
+        Args:
+            data (int): Payload
+            next_node (object Node): Porter to the next_node
+        """
+        self.data = data
+        self.next_node = next_node
 
     @property
-    def radius(self):
-        return self.__radius
+    def data(self):
+        """ Class getter for data
 
-    def area(self):
-        return self.__radius ** 2 * math.pi
+        Returns:
+            data (int): The payload node
+        """
+        return self.__data
 
-    def circumference(self):
-        return 2 * math.pi * self.__radius
+    @data.setter
+    def data(self, data):
+        """ Class setter for data
+        """
+        if not type(data) is int:
+            raise TypeError('data must be an integer')
+        self.__data = data
+
+    @property
+    def next_node(self):
+        """ Class getter for next_node
+
+        Returns:
+            next_node (Object): The next node
+        """
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, next_node):
+        """ Class setter for next_node
+        """
+        if (not type(next_node) is Node) and next_node is not None:
+            raise TypeError('next_node must be a Node object')
+        self.__next_node = next_node
+
+
+class SinglyLinkedList:
+    """ Class to support the Singly linked list
+    """
+
+    def __init__(self):
+        """ Class constructor
+        """
+        self.__head = None
+
+    def sorted_insert(self, value):
+        """ Ordered inserting method
+
+        Args:
+            value (int): The payload for the node
+        """
+        new = Node(value)
+        if self.__head is None:
+            new.next_node = None
+            self.__head = new
+        elif self.__head.data > value:
+            new.next_node = self.__head
+            self.__head = new
+        else:
+            tmp = self.__head
+            while (tmp.next_node is not None and tmp.next_node.data < value):
+                tmp = tmp.next_node
+            new.next_node = tmp.next_node
+            tmp.next_node = new
+
+    def __str__(self):
+        """ Formating list of date for print
+        """
+        values = []
+        tmp = self.__head
+        while tmp is not None:
+            values.append(str(tmp.data))
+            tmp = tmp.next_node
+        return ('\n'.join(values))
