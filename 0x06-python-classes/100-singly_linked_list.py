7eccbe9e2531a827e959260c9cdab9953aede8af
HamzaFikri <mohammed.hamza.fikri@gmail.comgit config --global user.name HamzaFikrigit config --global user.email mohammed.hamza.fikri@gmail.com>
2023-06-27 15:55:11 -0700

    Advanced tasks paython class

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,73 +1,22 @@
 #!/usr/bin/python3
-"""
-This is the "Single Linked List" module.
-
-Class Node takes in integer values as data within each node,
-and a next attribute which points to the next node or to None.
-
-Class SinglyLinkedList initializes a default head of None.
-Method sorted_insert handles all nodes created and adds them to
-the linked list sorted by the int value stored within.
-"""
+import dis
+import math
 
 
-class Node:
-    """A class that creates a single Node in a Linked List.
-    """
-    def __init__(self, data, next_node=None):
-        self.data = data
-        self.next_node = next_node
+class MagicClass:
+    def __init__(self, radius=0):
+        self.__radius = 0
+
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self.__radius = radius
 
     @property
-    def data(self):
-        return self.__data
+    def radius(self):
+        return self.__radius
 
-    @data.setter
-    def data(self, value):
-        if type(value) != int:
-            raise TypeError("data must be an integer")
-        self.__data = value
+    def area(self):
+        return self.__radius ** 2 * math.pi
 
-    @property
-    def next_node(self):
-        return self.__next_node
-
-    @next_node.setter
-    def next_node(self, value):
-        if not (value is None or type(value) is Node):
-            raise TypeError("next must be a Node object")
-        self.__next_node = value
-
-
-class SinglyLinkedList:
-    """A class that creates a Singly Linked List.
-    """
-    def __init__(self):
-        self.__head = None
-
-    def __repr__(self):
-        temp = self.__head
-        total = ""
-        while temp:
-            total += "{:d}".format(temp.data)
-            temp = temp.next_node
-            if temp:
-                total += "\n"
-        return total
-
-    def sorted_insert(self, value):
-        if self.__head is None:
-            self.__head = Node(value)
-        else:
-            curr = self.__head
-            prev = None
-            while curr and value > curr.data:
-                prev = curr
-                curr = curr.next_node
-            if curr is None:
-                prev.next_node = Node(value)
-            elif curr is self.__head and prev is None:
-                self.__head = Node(value, curr)
-            else:
-                newNode = Node(value, curr)
-                prev.next_node = newNode
+    def circumference(self):
+        return 2 * math.pi * self.__radius
