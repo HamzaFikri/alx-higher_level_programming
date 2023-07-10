8927576b261e2947e837fe02c7ca1ac5daf3164d
HamzaFikri <mohammed.hamza.fikri@gmail.comgit config --global user.name HamzaFikrigit config --global user.email mohammed.hamza.fikri@gmail.com>
2023-07-10 05:41:13 -0700

    python inheritance tasks

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""Defines an inherited list class MyList."""
+"""Class MyList that inherits from list"""
 
 
 class MyList(list):
-    """Implements sorted printing for the built-in list class."""
+    """prints the list sorted"""
 
     def print_sorted(self):
-        """Print a list in sorted ascending order."""
-        print(sorted(self))
+        """print the list sorted"""
+        new_list = self[:]
+        new_list.sort()
+        print("{}".format(new_list))
