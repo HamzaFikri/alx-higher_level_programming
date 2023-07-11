b92de3766fd95da2cf1c94a06eb51fcde9447e3a
HamzaFikri <mohammed.hamza.fikri@gmail.comgit config --global user.name HamzaFikrigit config --global user.email mohammed.hamza.fikri@gmail.com>
2023-07-11 02:59:48 -0700

    python input output tasks

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-'''Module for saving argv info via json to file.'''
+"""add an item"""
+from sys import argv
 import json
-import os.path
-import sys
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+import os
 
-file = "add_item.json"
-json_list = []
-if os.path.exists(file):
-    json_list = load_from_json_file(file)
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-for i in range(1, len(sys.argv)):
-    json_list.append(sys.argv[i])
 
-save_to_json_file(json_list, file)
+if os.path.isfile("add_item.json"):
+    my_list = load_from_json_file("add_item.json")
+else:
+    my_list = []
+
+for arg in range(1, len(argv)):
+    my_list.append(argv[arg])
+save_to_json_file(my_list, "add_item.json")
