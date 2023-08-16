07ad1a246d24ff437dbb181469c50c2f7b019b1c
HamzaFikri <mohammed.hamza.fikri@gmail.comgit config --global user.name HamzaFikrigit config --global user.email mohammed.hamza.fikri@gmail.com>
2023-08-16 06:50:04 -0700

    My privileges!

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
--- My privileges
--- show privs for user1
+-- Script that lists all privileges of the MySQL users user_0d_1 and user_0d_2.
+-- Show grants for user_0d_1
 SHOW GRANTS FOR user_0d_1@localhost;
--- show privs for user2
 SHOW GRANTS FOR user_0d_2@localhost;
