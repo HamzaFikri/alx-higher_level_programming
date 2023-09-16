e7d300398380a441ab6fd4c67cbd14b7c85785ae
HamzaFikri <mohammed.hamza.fikri@gmail.comgit config --global user.name HamzaFikrigit config --global user.email mohammed.hamza.fikri@gmail.com>
2023-09-16 07:34:25 -0700

    Contains a commit

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -2,26 +2,23 @@
 """
 Module that prints the first State object from the database hbtn_0e_6_usa
 """
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
-if __name__ == '__main__':
-    from sys import argv
-    from model_state import Base, State
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import sessionmaker
+if __name__ == "__main__":
+    engine = create_engine(
+                'mysql+mysqldb://{}:{}@localhost:3306/{}'
+                .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
-    if len(argv) != 4:
-        print("error")
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).\
+        filter(State.name == argv[4]).order_by(State.id).all()
+    if states:
+        print("{}".format(states[0].id))
     else:
-        USER = argv[1]
-        PASS = argv[2]
-        DB = argv[3]
-        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                               .format(USER, PASS, DB), pool_pre_ping=True)
-        Session = sessionmaker(bind=engine)
-        session = Session()
-        state_id = session.query(State).filter(State.name==argv[4]).first()
-        if state_id:
-            print(state_id[0].id)
-        else:
-            print("Not found")
-        session.close()
+        print("Not found")
+    session.close()
