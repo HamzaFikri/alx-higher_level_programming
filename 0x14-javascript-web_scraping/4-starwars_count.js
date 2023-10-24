4f61b08419d183a82e765c36cb363d125dc70503
Hamza Fikri <mohammed.hamza.fikri@gmail.com>
2023-10-24 15:14:19 +0100

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,7 +5,7 @@
   - The first argument is the API URL of the Star wars API:
     https://swapi-api.hbtn.io/api/films/
   - Wedge Antilles is character ID 18
-  - It is used the module request
+  - It is used the module request.
 
  */
 const request = require('request');
