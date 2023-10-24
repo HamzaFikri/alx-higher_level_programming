3f490dc289e0e7d95e0808391baf8a9e59713863
Hamza Fikri <mohammed.hamza.fikri@gmail.com>
2023-10-24 14:52:52 +0100

    write me

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// reads file passed as arg
+const fs = require('fs');
+
+fs.appendFile(process.argv[2], process.argv[3], function (err) {
+  if (err) {
+    console.log(err);
+  }
+});
