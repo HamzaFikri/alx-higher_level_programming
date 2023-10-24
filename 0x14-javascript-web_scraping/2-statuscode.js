ef1f54dac4a0bcbaa6d4a504a7c8cfd0bc6b31f4
Hamza Fikri <mohammed.hamza.fikri@gmail.com>
2023-10-24 15:13:43 +0100

    status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// makes get request and prints status code
+// makes get request and prints status code.
 const request = require('request');
 
 request.get(process.argv[2]).on('response', function (response) {
