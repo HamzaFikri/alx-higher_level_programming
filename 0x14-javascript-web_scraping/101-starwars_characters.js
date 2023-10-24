82f526a50600b6256a62c67fe3e09c192cd7419d
Hamza Fikri <mohammed.hamza.fikri@gmail.com>
2023-10-24 15:19:28 +0100

    Right order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -6,7 +6,7 @@
     https://swapi-api.hbtn.io/api/films/:id
   - Display one character name by line in the same order of the list characters
     in the /films/ response
-  - It is used the module request.
+  - It is used the module request
  */
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
