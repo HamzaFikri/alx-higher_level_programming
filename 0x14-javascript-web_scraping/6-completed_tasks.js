2a765153ec1f512ed489241d8623f980168a2fe5
Hamza Fikri <mohammed.hamza.fikri@gmail.com>
2023-10-24 15:18:31 +0100

    Update 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,7 +3,7 @@
   script that computes the number of tasks completed by user id.
   - The first argument is the API URL:
     https://jsonplaceholder.typicode.com/todos
-  - It is used the module request.
+  - It is used the module request
  */
 const request = require('request');
 const url = process.argv[2];
