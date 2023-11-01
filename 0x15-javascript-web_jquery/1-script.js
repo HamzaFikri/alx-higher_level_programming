35e71e423d0cf081b5b67a3dac4beca6c3943c67
HamzaFikri <mohammed.hamza.fikri@gmail.com>
2023-11-01 17:51:06 +0100

    With JQuery

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,9 @@
+/*
+  Write a Javascript script that updates the text color of the HTML tag HEADER
+  to red (#FF0000):
+  - It is used the jQuery API
+
+ */
+$(function () {
+  $('header').css('color', '#FF0000');
+});
