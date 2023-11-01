dbcb9b7faf7470ae9c587473061bd7732c4b109d
HamzaFikri <mohammed.hamza.fikri@gmail.com>
2023-11-01 17:53:43 +0100

    Star Wars movies

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,17 @@
+/*
+  script that fetches and lists all movies title by using this URL:
+  https://swapi-api.hbtn.io/api/films/?format=json
+  - All movie titles are listed in the HTML tag UL#list_movies
+  - It is used the jQuery API
+ */
+$.ajax({
+  url: 'https://swapi-api.hbtn.io/api/films/?format=json',
+  dataType: 'text',
+  success: function (data) {
+    let li = '<li></li>';
+    for (const movie of JSON.parse(data).results) {
+      li = '<li>' + movie.title + '</li>';
+      $('UL#list_movies').append(li);
+    }
+  }
+});
